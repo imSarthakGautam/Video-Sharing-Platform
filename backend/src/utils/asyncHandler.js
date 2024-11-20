716eafe764036fad8d3f41ee64d827b6eae0b341
imSarthakGautam <sarthakgtm175@gmail.com>
2024-11-20 14:58:15 +0545

    Utility functions bug fixes

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -1,34 +1,13 @@
 
 const asyncHandler = (requestHandler) =>{
-    //takes aync function as input.
+    //check asyncHandler.md if confused...
+    return (req, res, next)=>{
 
-
-    //returns a wrapped function :
-    // this wrapper catches errors that might occurs inside requestHandler
-    (req, res, next)=>{
-
-        Promise.resolve()
+        Promise.resolve(requestHandler(req,res,next))
         .catch((err)=>{next(err)})
         //if error occurs forwarded to next [express's error handler] for handling
     }
 }
 
+export {asyncHandler}
 
-
-
-//-------------------Approach 2----
-
- //const asyncHandler = (fn) =>{ ()=>{} }
- /*
- export const asyncHandler = (fn) => async (req, res, next)=>{
-    try{
-        await fn(req,res, next)
-
-    } catch (error){
-        res.status(error.code || 500).json({
-            success :false,
-            message : error.message
-        })
-    }
- }
-*/
--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -16,9 +16,10 @@ import { v2 as cloudinary } from 'cloudinary';
 
 // Configuration
 cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_API_SECRET, 
-    api_key: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_secret: process.env.CLOUDINARY_API_ENVIRONMENT_VARIABLE  // Click 'View API Keys' above to copy your API secret
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET  
+    // Click 'View API Keys' above to copy your API secret
 });
 
 // Upload an image
@@ -26,15 +27,16 @@ const uploadOnCloudinary = async (localfilePath) => {
     try{
         if (!localfilePath) return null
         //upload file in cloudinary
-        const response = await cloudinary.uploader
-        .upload(localfilePath, {
+        //console.log('uploading to cloudinary next...', localfilePath)
+        const response = await 
+        cloudinary.uploader.upload(localfilePath, {
                 resource_type: 'auto'
-            
-        })
+            })
 
         //file has been uploaded succesfully
-        console.log('File uploaded on cloudinary', response.url) // response = {url, public_id, secure_url, format}
-        
+        console.log('File uploaded on cloudinary', response.url) 
+        // response = {url, public_id, secure_url, format}
+        fs.unlinkSync(localfilePath)
         return response
 
     } catch (error) {
