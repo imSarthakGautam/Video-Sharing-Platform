80789b7804a284cfc1de8c2bd3e0ff2d23ecef60
imSarthakGautam <sarthakgtm175@gmail.com>
2024-11-20 14:57:31 +0545

    Api Error class fix

--- backend/src/utils/ApiError.js
+++ backend/src/utils/ApiError.js
@@ -10,7 +10,7 @@ class ApiError extends Error {
     constructor(
         statusCode,
         message= 'Something went wrong',
-        error=[],
+        errors=[],
         stack=""
     ){
         super(message)
