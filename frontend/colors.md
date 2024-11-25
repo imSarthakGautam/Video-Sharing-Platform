d1844e1ec7f218ecb813b7f300e5c861e041dccf
imSarthakGautam <sarthakgtm175@gmail.com>
2024-11-25 12:02:29 +0545

    Frontend Steup with Tailwind

---
+++ frontend/colors.md
@@ -0,0 +1,6 @@
+ YELLOW='[#deb522]'	
+ BLACK='[#0c0b00]'	
+ WHITE='[#fcf7f7]'	
+ BLUE='[#128bb5]'	
+
+
---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="bg-[#0c0b00] text-[#deb522]"> Video sharing platform </div>
+    
+  )
+}
+
+export default App
